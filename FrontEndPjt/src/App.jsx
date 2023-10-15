ef9dd0535dcb6dff8d65afa6898f33ac8bcc9d02
prefer_choi <prefer_choi@naver.com>
2023-10-16 02:24:16 +0900

    feat: front: get and show server list

--- FrontEndPjt/src/App.jsx
+++ FrontEndPjt/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from "axios";
+
 function App() {
+  const [serverList, setServerList] = useState({})
+  useEffect(()=>{
+    const getData = async () => {
+      try {
+        const res = await axios.get('http://localhost:8000/mainserver');
+        setServerList(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  },[])
+
   return (
     <>
-      
+      {Object.keys(serverList).map((server, index) => (
+        <div key={index}>
+          서버: {serverList[server]?.name} <br/>
+          서버 상태: {serverList[server]?.connect? '접속 가능' : '접속 불가능' } <br/>
+          접속인원: {serverList[server]?.roomsLength}/100
+          <hr/>
+        </div>
+      ))}
     </>
   )
 }
