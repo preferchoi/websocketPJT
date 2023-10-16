7890641fffb14da8ab39a1b4cebdf615a937c302
prefer_choi <prefer_choi@naver.com>
2023-10-16 15:07:05 +0900

    feat: front: socket.io-client set, lobby mount ws

--- FrontEndPjt/src/servers/Lobby.jsx
+++ FrontEndPjt/src/servers/Lobby.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+import io from 'socket.io-client';
+
 const Lobby = () => {
+    const [server, setServer] = useState('')
+    const { serverName } = useParams();
+    const [WS, setWS] = useState(null);
+
+    useEffect(() => {
+        const ws = io(`http://localhost:8000/${serverName}`);
+        setWS(ws);
+        console.log(ws);
+    }, []);
+
+    useEffect(() => {
+        if (WS) {
+            const getData = async () => {
+                try {
+                    const res = await axios.get(`http://localhost:8000/${serverName}/users`);
+                    setServer(res.data);
+                    console.log(res.data);
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+            getData();
+            return () => {
+                WS.disconnect();
+                setWS(null)
+            };
+        }
+    }, [WS]);
+
     return (
         <>
-        asdf
+            {server?.userList} / {serverName}
         </>
     )
 }
