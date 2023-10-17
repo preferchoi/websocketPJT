cdf44742c35c7f15479ba1343020fd09e4925ec9
prefer_choi <prefer_choi@naver.com>
2023-10-18 02:19:55 +0900

    feat: front: mainserver update btn add

--- FrontEndPjt/src/App.jsx
+++ FrontEndPjt/src/App.jsx
@@ -7,44 +7,64 @@ function App() {
 
   const [serverList, setServerList] = useState({})
   const [isGetData, setIsGetData] = useState(false)
+  const [coolTime, setCoolTime] = useState(0)
 
   const handleServerClick = (serverName) => {
     navigate(`/${serverName}`);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
+  }, [])
 
   const getData = async () => {
-      try {
-        const res = await axios.get('http://localhost:8000/mainserver');
-        setServerList(res.data);
-        console.log(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    try {
+      const res = await axios.get('http://localhost:8000/mainserver');
+      setServerList(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-      getData();
-    }, [])
-  
-    const handleGetData = () => {
-      setIsGetData(true)
-      getData();
-      setTimeout(() => setIsGetData(false), 3000);
-    };
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const handleGetData = () => {
+    setIsGetData(true)
+    getData();
+    setCoolTime(3)
+    setTimeout(() => {
+      setIsGetData(false)
+    }, 3000);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (coolTime > 0) {
+      intervalId = setInterval(() => {
+        setCoolTime((prevCoolTime) => prevCoolTime - 1);
+      }, 1000);
+    } else if (coolTime === 0) {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [coolTime])
 
   return (
     <>
-    <button onClick={handleGetData} disabled={isGetData}>서버 상태 갱신</button>
+    <p>
+      {coolTime}초 뒤 갱신 가능
+    </p>
+      <button onClick={handleGetData} disabled={isGetData}>서버 상태 갱신</button>
+      <hr />
       {Object.keys(serverList).map((server, index) => (
-        <div key={index} onClick={() => {serverList[server]?.connect ? handleServerClick(serverList[server]?.name) : alert('다른 서버를 선택해주세요.')}}>
-          서버: {serverList[server]?.name} <br/>
-          서버 상태: {serverList[server]?.connect? '접속 가능' : '접속 불가능' } <br/>
+        <div key={index} onClick={() => { serverList[server]?.connect ? handleServerClick(serverList[server]?.name) : alert('다른 서버를 선택해주세요.') }}>
+          서버: {serverList[server]?.name} <br />
+          서버 상태: {serverList[server]?.connect ? '접속 가능' : '접속 불가능'} <br />
           접속인원: {serverList[server]?.usersLength}/100
-          <hr/>
+          <hr />
         </div>
       ))}
     </>
