5293f9ae956cd59039d3786f407892b7d9e5f124
prefer_choi <prefer_choi@naver.com>
2023-10-25 16:41:25 +0900

    feat: front: room chat util add

--- FrontEndPjt/src/servers/Room.jsx
+++ FrontEndPjt/src/servers/Room.jsx
@@ -1,23 +1,55 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
 
 const Room = () => {
     const [WS, setWS] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+
+    const navigate = useNavigate();
+
     const { nspName, roomName } = useParams();
     useEffect(() => {
         const ws = io(`http://localhost:8000/${nspName}/${roomName}`);
         setWS(ws);
+        const addMessage = (message) => {
+            console.log("Received message: ", message);  // 이 로그가 출력되는지 확인
+            setMessages((prevMessages) => [...prevMessages, message]);
+        };
+        ws.on('receive_message', addMessage);
         return () => {
+            ws.off('receive_message', addMessage);
             ws.disconnect();
         };
     }, []);
+
+    const sendMessage = () => {
+        if (WS) {
+            WS.emit('send_message', newMessage);
+            setNewMessage("");
+        }
+    };
     return (
         <>
+            <p onClick={()=> {navigate(`/${nspName}`)}}>나가기</p>
             <h2>roomName: {roomName}</h2>
-            <p>
-                생성된 방 들어옴
-            </p>
+
+            <div>
+                <h3>채팅 로그</h3>
+                {messages.map((message, index) => (
+                    <div key={index}>{message}</div>
+                ))}
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send</button>
+            </div>
         </>
     )
 }
